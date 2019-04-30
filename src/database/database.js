c15e06fd01d094ed4138496e527c3c476260e202
FelipeOliveira <felipe.boliveira@outlook.com>
2019-04-30 00:46:07 -0300

    Povoa a database em memória de alunos e cursos

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 const escola = {
     cursos: [
         {
-            nome: 'Sistemas de Informações',
+            nome: 'Sistemas de Informação',
             alunos: [
                 {
                     nome:"Matheus", 
@@ -20,8 +20,73 @@ const escola = {
                     matriculaAtiva:true,
                     estadoCivil:"solteiro",
                     matricula:"5306312",
-                    telefone: ["66666666", "977777777"],
+                    telefone: ["27723590", "976847774"],
                     idade:25
+                },
+                {
+                    nome:"Miguel",
+                    id:"3",
+                    dataNascimento:"28/08/1989",
+                    matriculaAtiva:true,
+                    estadoCivil:"casado",
+                    matricula:"5306320",
+                    telefone: ["35689652", "978545896"],
+                    idade:30
+                },
+                {
+                    nome:"Ana Paula",
+                    id:"4",
+                    dataNascimento:"05/10/1999",
+                    matriculaAtiva:true,
+                    estadoCivil:"solteiro",
+                    matricula:"5306314",
+                    telefone: ["636728547", "975354589"],
+                    idade:20
+                }
+            ]
+        },
+        {
+            nome: 'Análise de Sistemas',
+            alunos: [
+                {
+                    nome:"Marcos", 
+                    id:"5",
+                    dataNascimento:"25/01/1990",
+                    matriculaAtiva:true,
+                    estadoCivil:"casado",
+                    matricula:"5303310",
+                    telefone: ["27728547", "975891990"],
+                    idade:29
+                },
+                {
+                    nome:"João",
+                    id:"6",
+                    dataNascimento:"01/12/1995",
+                    matriculaAtiva:true,
+                    estadoCivil:"solteiro",
+                    matricula:"5303300",
+                    telefone: ["27723548", "976877458"],
+                    idade:24
+                },
+                {
+                    nome:"Anderson",
+                    id:"7",
+                    dataNascimento:"14/05/1998",
+                    matriculaAtiva:true,
+                    estadoCivil:"solteiro",
+                    matricula:"5303220",
+                    telefone: ["27727690", "988147458"],
+                    idade:21
+                },
+                {
+                    nome:"Daniela",
+                    id:"8",
+                    dataNascimento:"25/03/2001",
+                    matriculaAtiva:true,
+                    estadoCivil:"solteiro",
+                    matricula:"5303254",
+                    telefone: ["27825478", "976585422"],
+                    idade:18
                 }
             ]
         }
