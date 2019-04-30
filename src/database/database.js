28637588e76c9a0ce6196ad896f3b87f715a0992
momanhaes <momanhaes@gmail.com>
2019-04-29 21:13:51 -0300

    Cria database em memória de alunos e cursos

--- src/database/database.js
+++ src/database/database.js
@@ -1,29 +1,31 @@
 const escola = {
-    cursos:[
-
-    ],
-    alunos:[
+    cursos: [
         {
-            nome:"Matheus", 
-            id:"1",
-            dataNascimento:"07/11/1995",
-            matriculaAtiva:true,
-            estadoCivil:"solteiro",
-            matricula:"5306315",
-            telefone: ["26335666", "986941990"],
-            idade:23
-        },
-        {
-            nome:"Felipe",
-            id:"2",
-            dataNascimento:"19/07/1993",
-            matriculaAtiva:true,
-            estadoCivil:"solteiro",
-            matricula:"5306312",
-            telefone: ["66666666", "977777777"],
-            idade:25
+            nome: 'Sistemas de Informações',
+            alunos: [
+                {
+                    nome:"Matheus", 
+                    id:"1",
+                    dataNascimento:"07/11/1995",
+                    matriculaAtiva:true,
+                    estadoCivil:"solteiro",
+                    matricula:"5306315",
+                    telefone: ["26335666", "986941990"],
+                    idade:23
+                },
+                {
+                    nome:"Felipe",
+                    id:"2",
+                    dataNascimento:"19/07/1993",
+                    matriculaAtiva:true,
+                    estadoCivil:"solteiro",
+                    matricula:"5306312",
+                    telefone: ["66666666", "977777777"],
+                    idade:25
+                }
+            ]
         }
-    ],
+    ]
 };
 
 module.exports = escola;
